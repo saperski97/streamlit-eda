59b80dd4cdf05a4c62ebf99f3a4b6f90cfdb1bbf
saperski97 <49766326+saperski97@users.noreply.github.com>
2024-05-31 15:00:37 +0200

    chamge PyGWalker theme to light

--- src/App.py
+++ src/App.py
@@ -16,7 +16,7 @@ def get_profile_report(tbl):
 
 @st.cache_resource
 def get_streamlit_renderer(tbl):
-    return StreamlitRenderer(tbl, dark="light")
+    return StreamlitRenderer(tbl, appearance="light")
 
 
 def get_db_connection():
